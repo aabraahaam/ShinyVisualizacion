950078b5be309aee0f95b2460a2b1d07ab57a1a1
aabraahaam <32160387+aabraahaam@users.noreply.github.com>
2017-12-18 15:25:26 +0100

    Pinta distribuciones

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shinydashboard)
 library(gridExtra)
 library(ggplot2)
+library(triangle)
 
 ui <- dashboardPage(
   dashboardHeader(),
@@ -15,41 +16,48 @@ ui <- dashboardPage(
       condition = "input.radio == '1'",
       textInput('nomd','Nombre de la distribución'),
       selectInput("select", label = "Distribución", 
-                  choices = list( " " = 9,"Normal" = 1, "Normal Truncada" = 2, "Lognormal" = 3,
-                                 "Binomial" = 4, "Gamma" = 5, "Chi Cuadrada" = 6,
-                                 "Poisson" = 7, "Uniforme" = 8, "Triangular" = 10),
+                  choices = list( " " = 9,"Normal" = 'rnorm',
+                                 "Normal Truncada" ='normt', 
+                                 "Lognormal" = 'lognorm',
+                                 "Binomial" = 'rbinom',
+                                 "Gamma" = 'rgamma', 
+                                 "Chi Cuadrada" = 'rchisq',
+                                 "Poisson" = 'rpois',
+                                 "Uniforme" = 'runif', 
+                                 "Triangular" = 'rtriang'),
                   selected = 9),
+      #Distribuciones----
       #normal
       conditionalPanel(
-        condition = "input.select =='1' || input.select =='2' || input.select =='3'",
+        condition = "input.select =='rnorm' || input.select =='normt' || input.select =='lognorm'",
         numericInput('mediaN','Media',0),
         numericInput('sigmaN','Varianza',1)
       ),
       #binomial
       conditionalPanel(
-        condition = "input.select =='4'",
+        condition = "input.select =='rbinom'",
         numericInput('nBin','Número de eventos',10),
         numericInput('pBin','Probabilidad',.5)
       ),
       #Gamma
       conditionalPanel(
-        condition = "input.select =='5'",
+        condition = "input.select =='rgamma'",
         numericInput('kGam','k',10),
         numericInput('lGam',HTML("&lambda;"),.5)
       ),
       #Chi cuadrada
       conditionalPanel(
-        condition = "input.select =='6'",
+        condition = "input.select =='rchisq'",
         numericInput('kChi','Grados de libertad',10)
       ),
       #poisson
       conditionalPanel(
-        condition = "input.select =='7'",
+        condition = "input.select =='rpois'",
         numericInput('lPoi',HTML("&lambda;"),.5)
       ),
       #uniforme
       conditionalPanel(
-        condition = "input.select =='8'",
+        condition = "input.select =='runif'",
         numericInput('aUni','Mínimo',0),
         numericInput('bUni','Máximo',10)
       ),
@@ -57,7 +65,7 @@ ui <- dashboardPage(
       #‘triangle'
       #rtriangle(n, a=0, b=1, c=(a+b)/2)
       conditionalPanel(
-        condition = "input.select =='10'",
+        condition = "input.select =='rtriang'",
         numericInput('aTri','Mínimo',0),
         numericInput('bTri','Máximo',10),
         numericInput('cTri','Moda',6)
@@ -77,62 +85,33 @@ ui <- dashboardPage(
   )
 )
 
-
+#server----
 server <- function(input, output, session) {
   
-  df <- reactiveValues(main = as.data.frame(NULL))
-  
-  observeEvent(input$agregar,{
-    if(input$select == 1){
-      df$main[1:1000,1] <- as.data.frame(rnorm(1000,input$mediaN,input$sigmaN))
-      colnames(df$main)[1] <- input$nomd
-    }else{
-      df$main <- NULL
-    }
-  })
-  
   output$grafica <- renderPlot({
-    # a <- as.numeric(input$mediaN)
-    # b <- as.numeric(input$sigmaN)
-    # df$main <- as.data.frame(rnorm(100,0,1))
-    if (dim(df$main)[1] == 0){
-      
-    }else{
-    ggplot(df$main,aes(x=df$main[,1])) + geom_histogram(bins = 20) + ggtitle(input$nomd)
-    }
+    
+    df <- data.frame(x=curdata$variable1)
+    
+    ggplot(df,aes(x=x)) + geom_histogram(bins = 20) + ggtitle(input$nomd)
+    
   })
 
-
-
-  # observeEvent(input$agregar,{
-  #   curdata <- reactive({
-  #     dist <- switch(input$select, 
-  #            norm = rnorm, 
-  #            # '2' = runif, 
-  #            # '3' = rexp,
-  #            rnorm)
-  #     dist(100)
-  #   })
-  # #   dist <- switch(input$select,
-  # #                  1 = rnorm,
-  # #                  2 = runif,
-  # #                  3 = rlnorm,
-  # #                  4 = rexp,
-  # #                  rnorm)
-  # # 
-  # #   df$main <-dist(input$n)
-  #   #df$main <- as.data.frame(rnorm(1000,0,1))
-  # df$main <- as.data.frame(curdata)
-  # })
-  # 
-  # output$grafica <- renderPlot({
-  #   if (is.null(df$main)){
-  #     
-  #   }else{
-  #     ggplot(df$main,aes(x=df$main[,1])) + geom_histogram(bins = 20) 
-  #     }
-  # 
-  # })
+  curdata <- reactiveValues()
+  observeEvent(input$agregar,{
+    
+    curdata$variable1 <- switch(input$select,
+             'rnorm' = rnorm(1000,input$mediaN,input$sigmaN),
+             'normt' = rnorm(1000,input$mediaN,input$sigmaN),
+             'lognorm' = rnorm(1000,input$mediaN,input$sigmaN),
+             'rbinom' = rbinom(1000,input$nBin,input$pBin),
+             'rgamma' = rgamma(1000,input$kGam,input$lGam),
+             'rchisq' = rchisq(1000,input$kChi),
+             'rpois' = rpois(1000,input$lPoi),
+             'runif' = runif(1000,input$aUni,input$bUni),
+             'rtriang' = rtriangle(1000,input$aTri,input$bTri,input$cTri),
+             NULL)
+      
+  })
   
 }
 
